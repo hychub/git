9a91e0477be582c2ca934a3a5620c91dd4177fd5
shiqi <hxc18420357915@163.com>
2022-09-24 18:36:27 +0800

    修复版本1.01.02

--- src/main/java/com/git/test/GitDemo.java
+++ src/main/java/com/git/test/GitDemo.java
@@ -1,7 +1,8 @@
 package com.git.test;
 
 public class GitDemo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("原始版本1.01.01");
+        System.out.println("修复版本1,01.02");
     }
 }
