51d0f6e4f5440e7366107e5f934ef57efd693414
shiqi <hxc18420357915@163.com>
2022-09-24 18:57:47 +0800

    修复功能版本1.02.01

--- src/main/java/com/git/test/GitDemo.java
+++ src/main/java/com/git/test/GitDemo.java
@@ -5,5 +5,10 @@ public class GitDemo {
         System.out.println("原始版本1.01.01");
         System.out.println("修复版本1,01.02");
         System.out.println("修复版本1,01.03");
+        function_fix();
+    }
+
+    public static void function_fix(){
+        System.out.println("修复功能版本1.02.01");
     }
 }
